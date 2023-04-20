e863cccfbbde2d6b5a4c4f93c99dd3c42baa3783
juls <jpsierra316@misena.edu.co>
2023-04-20 11:57:17 -0500

    estilos agregados

--- app/Views/principal/header.php
+++ app/Views/principal/header.php
@@ -2,6 +2,9 @@
 <html lang="en">
 
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,400;1,500&display=swap" rel="stylesheet">
     <title>Ingecosmo Ltda.</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -16,26 +19,41 @@
 
     <div class="d-flex">
         <nav id="sidebar" class="active">
-            <h1><a href="#" class="logo">M.</a></h1>
+            <!-- <h1><a class="logo"></a></h1> -->
             <ul class="list-unstyled components mb-5">
                 <li class="active">
-                    <a href="#"><span class="fa fa-home"></span> Home</a>
+                    <a href="#" style=""><span><img style=" width:40px; height:40px; " src="<?php echo base_url('/img/usuario.png') ?>"/></span><p id="pa">Almacenista</p></a>
                 </li>
                 <li>
-                    <a href="#"><span class="fa fa-user"></span> About</a>
+                    <a href="#"><span><img style=" width:40px; height:40px; " src="<?php echo base_url('/img/trabajadores.png') ?>"/></span>Trabajadores</a>
                 </li>
                 <li>
-                    <a href="#"><span class="fa fa-sticky-note"></span> Blog</a>
+                    <a href="#"><span><img style=" width:40px; height:40px; " src="<?php echo base_url('/img/clientes.png') ?>"/></span> Clientes</a>
                 </li>
                 <li>
-                    <a href="#"><span class="fa fa-cogs"></span> Services</a>
+                    <a href="#"><span><img style=" width:40px; height:40px; " src="<?php echo base_url('/img/materiales.png') ?>"/></span>Materiales</a>
                 </li>
                 <li>
-                    <a href="#"><span class="fa fa-paper-plane"></span> Contacts</a>
+                    <a href="#"><span><img style=" width:40px; height:40px; " src="<?php echo base_url('/img/vehiculo.png') ?>"/></span> Vehiculos</a>
+                </li>
+                <li>
+                    <a href="#"><span><img style=" width:40px; height:40px; " src="<?php echo base_url('/img/proveedores.png') ?>"/></span> Proveedores</a>
+                </li>
+                <li>
+                    <a href="#"><span><img style=" width:35px; height:40px; " src="<?php echo base_url('/img/historial.png') ?>"/></span> Historial</a>
+                </li>
+                <li>
+                    <a href="#"><span><img style=" width:40px; height:40px; " src="<?php echo base_url('/img/estanteria.png') ?>"/></span> Estanteria</a>
+                </li>
+                <li>
+                    <a href="#"><span><img style=" width:40px; height:40px; " src="<?php echo base_url('/img/usuarioS.png') ?>"/></span> Usuarios</a>
+                </li>
+                <li>
+                   <a href="#"><span><img style=" width:35px; height:40px; " src="<?php echo base_url('/img/salir.png') ?>"/></span> Cerrar Sesion</a>
                 </li>
             </ul>
             <nav>
-                <div class="container-fluid d-flex justify-content-center">
+                <div class="container-fluid d-flex justify-content-center" style="position:relative; bottom: 10px">
                     <button type="button" id="sidebarCollapse" class="btn btn-primary">
                         <i class="fa fa-bars"></i>
                         <span class="sr-only">Toggle Menu</span>
@@ -43,6 +61,7 @@
                 </div>
             </nav>
         </nav>
+        <a href=""><img  class="log" id="log" style="" src="<?php echo base_url('/img/ingecosmo.png') ?>"/></a>
 
     </div>
 
@@ -51,5 +70,12 @@
     <script src="js/bootstrap.min.js"></script>
     <script src="js/main.js"></script>
 </body>
+
+
+<script>
+    function accion(){
+        
+    }
+</script>
 
 </html>
--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,34 @@
-(function($) {
+$(document).ready(function ($) {
+	if (document.getElementById("sidebar").className == 'active') {
+		document.getElementById("pa").style.opacity = '0'
+		document.getElementById("pa").style.display = 'none'
+	}else{
+		document.getElementById("pa").style.opacity = '1'
+		document.getElementById("pa").style.display = 'block'
+		document.getElementById("pa").style.padding = '10px 30px'
+	}
 
-	"use strict";
-
-	var fullHeight = function() {
+	var fullHeight = function () {
 
 		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
+		$(window).resize(function () {
 			$('.js-fullheight').css('height', $(window).height());
 		});
 
 	};
 	fullHeight();
 
-	$('#sidebarCollapse').on('click', function () {
-      $('#sidebar').toggleClass('active');
-  });
 
-})(jQuery);
+	$('#sidebarCollapse').on('click', function () {
+		$('#sidebar').toggleClass('active');
+		if (document.getElementById("sidebar").className == 'active') {
+			document.getElementById("pa").style.opacity = '0'
+			document.getElementById("pa").style.display = 'none'
+		}else{
+			document.getElementById("pa").style.opacity = '1'
+			document.getElementById("pa").style.display = 'block'
+		}
+	});
+
+
+})
