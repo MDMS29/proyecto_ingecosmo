9139f809b079cf409fac0a60a3bd943a7b0825a5
MDMS29 <mdmazo30@misena.edu.co>
2023-04-20 19:08:26 -0500

    Modal Agregar

--- app/Views/principal/sidebar.php
+++ app/Views/principal/sidebar.php
@@ -7,10 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
-
+    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/usuarios.css">
+    <link rel="stylesheet" href="<?php echo base_url('bootstrap5/css/bootstrap.min.css') ?>"></link>
+    <link rel="stylesheet" href="<?php echo base_url('css/style.css') ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/usuarios.css')?>">
+
+    <!-- SCRIPTS -->
+    <script src="<?php echo base_url('bootstrap5/js/bootstrap.bundle.min.js') ?>"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/main.js"></script>
 </head>
 
 <body class=" d-flex align-items-stretch">
@@ -50,10 +56,6 @@
 
     </div>
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/popper.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
 </body>
 
 </html>
--- app/Views/usuarios/usuarios.php
+++ app/Views/usuarios/usuarios.php
@@ -24,12 +24,93 @@
                         <td class="text-center"><?= $d['n_identificacion'] ?></td>
                         <td class="text-center"><?= $d['nombre_rol'] ?></td>
                         <td class="text-center">
-                            <a class="btn"><img src="<?php echo base_url('icons/edit.svg')?>" alt="Boton Editar" title="Editar Usuario"></a>
-                            <a class="btn"><img src="<?php echo base_url('icons/delete.svg')?>" alt="Boton Eliminar" title="Eliminar Usuario"></a>
+                            <button class="btn" onclick="seleccionarUsuario(<?= $d['id_usuario'] ?>)" data-bs-target="#agregarUsuario" data-bs-toggle="modal"><img src="<?php echo base_url('icons/edit.svg') ?>" alt="Boton Editar" title="Editar Usuario"></button>
+
+                            <button class="btn"><img src="<?php echo base_url('icons/delete.svg') ?>" alt="Boton Eliminar" title="Eliminar Usuario"></button>
                         </td>
                     </tr>
                 <?php } ?>
             </tbody>
         </table>
     </div>
+    <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#agregarUsuario" onclick="seleccionarUsuario(<?php echo 1 . ',' . 1 ?>)"><i class="bi bi-clipboard-plus"></i> Agregar</button>
 </div>
+
+<form method="POST" action="<?php echo base_url('instrUsu'); ?>" autocomplete="off" id="formularioUsuarios">
+    <div class="modal fade" id="agregarUsuario" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <input type="text" name="id" id="id" value="0" hidden>
+        <input type="text" name="tp" id="tp" hidden>
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="tituloModal">Agregar Usuario</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="d-flex column-gap-3" style="width: 100%">
+                            <div class="mb-3" style="width: 100%">
+                                <label for="nombres" class="col-form-label">Nombres:</label>
+                                <input type="text" name="nombres" class="form-control" id="nombres">
+                            </div>
+                            <div class="mb-3" style="width: 100%">
+                                <label for="apellidos" class="col-form-label">Apellidos:</label>
+                                <input type="text" name="apellidos" class="form-control" id="apellidos">
+                            </div>
+                        </div>
+                        <div class="d-flex column-gap-3" style="width: 100%">
+                            <div class="mb-3" style="width: 100%">
+                                <label for="nombres" class="col-form-label"># Identificacíon:</label>
+                                <input type="number" name="n_iden" class="form-control" id="n_iden">
+                            </div>
+                            <div class="mb-3" style="width: 100%">
+                                <label for="apellidos" class="col-form-label">Rol:</label>
+                                <select class="form-select" name="rol" id="rol">
+                                    <option value="" selected> -- Seleccione un Rol --</option>
+                                    <option value="1">Super Administrador</option>
+                                    <option value="2">Administrador</option>
+                                </select>
+                                <!-- <input type="email" name="email" class="form-control" id="email"> -->
+                            </div>
+                        </div>
+                        <div class="mb-3" style="width: 100%">
+                            <label for="apellidos" class="col-form-label">Email:</label>
+                            <input type="email" name="email" class="form-control" id="email">
+                        </div>
+                        <div class="d-flex column-gap-3" style="width: 100%">
+                            <div class="mb-3" style="width: 100%">
+                                <label for="nombres" class="col-form-label">Contraseña:</label>
+                                <input type="password" name="contra" class="form-control" id="contra" minlength="5">
+                                <small class="normal">¡La contraseña debe contar con un minimo de 6 caracteres!</small>
+                            </div>
+                            <div class="mb-3" style="width: 100%">
+                                <div>
+                                    <label for="nombres" class="col-form-label">Confirmar Contraseña:</label>
+                                    <input type="password" name="confirContra" class="form-control" id="confirContra" minlength="5">
+                                </div>
+                                <small id="msgConfir" class="normal"></small>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-success" id="btnGuardar">Guardar</button>
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+
+
+
+<script>
+    function seleccionarUsuario(id, tp) {
+        if(tp == 2){
+            alert('Editar')
+
+        }else{
+            // alert('Insertar')
+        }
+    }
+</script>
