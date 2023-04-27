060f5a8bd5f7f8af73439cea63eb5b52d0ff8b3e
Ninja <dxpanda07@gmail.com>
2023-04-27 09:42:16 -0500

    Login

--- app/Controllers/Usuarios.php
+++ app/Controllers/Usuarios.php
@@ -32,23 +32,30 @@ class Usuarios extends BaseController
     }
     public function insertar()
     {
-
         $tp = $this->request->getPost('tp');
-        $id = $this->request->getPost('id');
-        $nombreP = $this->request->getPost('nombre_p');
-        $nombreS = $this->request->getPost('nombre_s');
-        $apellidoP = $this->request->getPost('apellido_p');
-        $apellidoS = $this->request->getPost('apellido_s');
+        $idUser = $this->request->getPost('id');
+        $nombreP = $this->request->getPost('nombreP');
+        $nombreS = $this->request->getPost('nombreS');
+        $apellidoP = $this->request->getPost('apellidoP');
+        $apellidoS = $this->request->getPost('apellidoS');
         $tipoDoc = $this->request->getPost('tipoDoc');
         $nIdenti = $this->request->getPost('nIdenti');
-        $rol = $this->request->getPost('rol');
+        $rol = $this->request->getVar('rol');
         $contra = $this->request->getVar('contra');
-
-
 
         if ($tp == 2) {
             //Actualizar datos
-
+            $usuarioUpdate = [
+                'id_rol' => $rol,
+                'tipo_doc' => $tipoDoc,
+                'n_identificacion' => $nIdenti,
+                'nombre_p' => $nombreP,
+                'nombre_s' => $nombreS,
+                'apellido_p' => $apellidoP,
+                'apellido_s' => $apellidoS
+            ];
+            $this->usuarios->update($idUser, $usuarioUpdate);
+            return $idUser;
         } else {
             //Insertar datos
             //Si la respuesta esta vacia - guardar
@@ -63,62 +70,29 @@ class Usuarios extends BaseController
                 'contrasena' => password_hash($contra, PASSWORD_DEFAULT)
             ];
             $this->usuarios->save($usuarioSave);
-            return redirect()->to(base_url('usuarios'));
+            return json_encode($this->usuarios->getInsertID());
         }
     }
-
-    public function buscarUsuario($id, $nIdenti)
-    {
-        // echo $nIdenti;
-        $array = array();
-
-        if ($id != 0) {
-            $data = $this->usuarios->buscarUsuario($id, 0);
-            if (!empty($data)) {
-                array_push($array, $data);
-                return json_encode($array);
-            }
-        } else if ($nIdenti != 0) {
-            $data = $this->usuarios->buscarUsuario(0, $nIdenti);
-            array_push($array, $data);
-            return json_encode($array);
-        } else if ($id != 0 && $nIdenti != 0) {
-            $data = $this->usuarios->buscarUsuario($id, $nIdenti);
-            array_push($array, $data);
-            return json_encode($array);
-        }
-    }
-
     public function login()
     {
         $nIdenti = $this->request->getPost('usuario');
         $contrasena = $this->request->getVar('contrasena');
         $datos = $this->usuarios->buscarUsuario(0, $nIdenti);
-
-
-        // $Usuario = new Usuarios();
+        $textoAlerta="<div class='alerta'> <i class='bi bi-exclamation-circle-fill'></i> Usuario o Contraseña Incorrecta </div>";
         if (!empty($datos) && password_verify($contrasena, $datos['contrasena'])) {
-
-            echo $nIdenti;
-
-            //Aqui puedes meter toda la info del user que aparcera en el home
-
             $data = [
+                "id" => $datos['id_usuario'],
                 "nombre" => $datos['nombre_p'],
                 "apellido" => $datos['apellido_p'],
                 "rol" => $datos['nombre_rol']
             ];
-
             $session = session();
-            $session->set($data); //agregamos los datos obtenidos del usuario
-
+            $session->set($data); 
             return redirect()->to(base_url('/home'));
         } else {
-            return redirect()->to(base_url('/'));
+            return redirect()->to(base_url('/'))->with('mensaje', $textoAlerta);
         }
     }
-
-
     public function salir()
     {
         $session = session();
