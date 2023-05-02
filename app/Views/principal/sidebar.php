f8ec98a23ee2a0ef746ddb840619a391fe5a93d6
MDMS29 <126972569+Dulfran1984@users.noreply.github.com>
2023-05-02 11:06:06 -0500

    v1

--- app/Views/principal/sidebar.php
+++ app/Views/principal/sidebar.php
@@ -9,23 +9,24 @@
     <title>Ingecosmo Ltda.</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+    <link rel="stylesheet" href="<?php echo base_url('bootstrap5/css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?php echo base_url('css/style.css') ?>">
     <link rel="stylesheet" href="<?php echo base_url("css/principal/home.css") ?>">
-    <link rel="stylesheet" href="<?php echo base_url('bootstrap5/css/bootstrap.min.css') ?>">
-    <link rel="stylesheet" href="<?= base_url('bootstrap5/bootstrap.min.css') ?>" />
-
     <link rel="stylesheet" href="<?= base_url('dataTable/dataTables.bootstrap5.min.css') ?>" />
+
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
     <!-- SCRIPTS -->
     <script src="<?= base_url('js/jquery.min.js') ?>"></script>
-    <script src="<?= base_url('js/main.js') ?>"></script>
-    
-    <script src="<?= base_url('bootstrap5/bootstrap.bundle.min.js') ?>"></script>
-
     <script src="<?= base_url('dataTable/jquery.dataTables.js') ?>"></script>
     <script src="<?= base_url('dataTable/dataTables.bootstrap5.min.js') ?>"></script>
+    <script src="<?= base_url('js/main.js') ?>"></script>
+
 
 
 </head>
