4387037f935910e331d6c0e4982163e20c8d84cb
MDMS29 <126972569+Dulfran1984@users.noreply.github.com>
2023-05-03 11:53:14 -0500

    v1

--- app/Controllers/Proveedores.php
+++ app/Controllers/Proveedores.php
@@ -22,32 +22,33 @@ class Proveedores extends BaseController
     }
 
     public function insertar()
-        {
-            $tp=$this->request->getPost('tp');
-            $tipoTercero = 8;
-            $tipoDocumento = 2;
-            if ($this->request->getMethod() == "post") {
-                if ($tp == 1) {
-                    $this->proveedores->save([
+    {
+        $tp = $this->request->getPost('tp');
+        $tipoTercero = 8;
+        $tipoDocumento = 2;
+        if ($this->request->getMethod() == "post") {
+            if ($tp == 1) {
+                $this->proveedores->save([
+                    'razon_social' => $this->request->getPost('RazonSocial'),
+                    'n_identificacion' => $this->request->getPost('nit'),
+                    'direccion' => $this->request->getPost('direccion'),
+                    'tipo_tercero' => $tipoTercero,
+                    'tipo_doc' => $tipoDocumento
+                ]);
+            } else {
+                $this->proveedores->update(
+                    $this->request->getPost('id'),
+                    [
                         'razon_social' => $this->request->getPost('RazonSocial'),
                         'n_identificacion' => $this->request->getPost('nit'),
                         'direccion' => $this->request->getPost('direccion'),
                         'tipo_tercero' => $tipoTercero,
                         'tipo_doc' => $tipoDocumento
-                    ]);
-                } else {
-                    $this->proveedores->update($this->request->getPost('id'),
-                    [                    
-                        'razon_social' => $this->request->getPost('RazonSocial'),
-                        'n_identificacion' => $this->request->getPost('nit'),
-                        'direccion' => $this->request->getPost('direccion'),
-                        'tipo_tercero' => $tipoTercero,
-                        'tipo_doc' => $tipoDocumento
-                    ]);
-                }
-                return redirect()->to(base_url('/proveedores'));
+                    ]
+                );
             }
-        
+            return redirect()->to(base_url('/proveedores'));
+        }
     }
 
     public function buscarProveedor($id)
@@ -66,13 +67,12 @@ class Proveedores extends BaseController
         return redirect()->to(base_url('/proveedores'));
     }
 
-    public function eliminados(){
+    public function eliminados()
+    {
         $proveedores = $this->proveedores->where('estado', 'E')->findAll();
-        
+
         $data = ['proveedores' => $proveedores];
         echo view('/principal/sidebar');
         echo view('/proveedores/eliminados', $data);
     }
-
-
 }
