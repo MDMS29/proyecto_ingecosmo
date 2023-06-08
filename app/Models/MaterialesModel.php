79f50009f19754668fbd0045143b0b3d006db186
Shadia <bryan.andion@gmail.com>
2023-06-08 11:05:13 -0500

    arreglado filas

--- app/Models/MaterialesModel.php
+++ app/Models/MaterialesModel.php
@@ -39,7 +39,7 @@ class MaterialesModel extends Model
     {
         $this->select('materiales.* ,estanteria.nombre as nombreEstante, filas.nombre as filaNombre');
         $this->join('estanteria', 'estanteria.id = materiales.estante');
-        $this->join('filas', 'filas.id_estante = materiales.fila');
+        $this->join('filas', 'filas.id_fila = materiales.fila');
 
         $this->where('id_material', $id_material);
         $datos = $this->first();
