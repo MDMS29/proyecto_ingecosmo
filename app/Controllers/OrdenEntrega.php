a725fc4078af5a4caf4c49150b2cb93c6e7d69ba
YuleAvilez <>
2023-06-21 11:57:05 -0500

    Orden de servicio- Agregar

--- app/Controllers/OrdenEntrega.php
+++ app/Controllers/OrdenEntrega.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController; /*la plantilla del controlador general de codeigniter */
 use App\Models\MoviEncModel;
+use App\Models\MoviDetModel;
 use App\Models\MaterialesModel;
 use App\Models\ParamModel;
 use App\Models\VehiculosModel;
@@ -15,7 +16,7 @@ use App\Models\EstanteriaModel;
 class OrdenEntrega extends BaseController
 {
 
-    protected $ordenEntrega;
+    protected $ordenEntrega, $movDet;
     protected $materiales;
     protected $trabajadores;
     protected $vehiculos;
@@ -27,6 +28,7 @@ class OrdenEntrega extends BaseController
     public function __construct()
     {
         $this->ordenEntrega = new MoviEncModel();
+        $this->movDet = new MoviDetModel();
         $this->trabajadores = new TrabajadoresModel();
         $this->materiales = new MaterialesModel();
         $this->vehiculos = new VehiculosModel();
@@ -36,6 +38,11 @@ class OrdenEntrega extends BaseController
         $this->estanteria = new EstanteriaModel();
     }
 
+    public function obtenerOrdenEntrega()
+    {
+        $res = $this->ordenEntrega->ordenesEntrega();
+        return json_encode($res);
+    }
     public function index()
     {
         $trabajadores = $this->trabajadores->trabajadoresOrdenes();
@@ -50,42 +57,15 @@ class OrdenEntrega extends BaseController
         echo view('/ordenEntrega/ordenEntrega', $data);
     }
 
-    public  function mostrarOrdenesEntrega($id, $nombre, $idCate)
-    {
-        $ordenEntrega = $this->ordenEntrega->ordenesEntrega();
-        $materiales = $this->materiales->obtenerMateriales($id);
-        $trabajadores = $this->trabajadores->trabajadoresOrdenes();
-        $vehiculos = $this->vehiculos->vehiculosOrdenes();
-        $estanteria = $this->estanteria->obtenerBodega();
-        if (empty($materiales)) {
-            $materiales = '';
-        }
-
-        $data = ['data' => $ordenEntrega, 'nombreCategoria' => $nombre, 'idCate' => $idCate, "vehiculos" => $vehiculos, "trabajadores" => $trabajadores, "estanteria" => $estanteria,];
-        echo view('/principal/sidebar');
-
-        echo view('/ordenEntrega/ordenEntrega', $data);
-    }
-
-
-
-    public function detallesOrden($id)
-    {
-        $returnData = array();
-        $ordenEntrega_ = $this->ordenEntrega->traerDetalles($id);
-        if (!empty($ordenEntrega_)) {
-            array_push($returnData, $ordenEntrega_);
-        }
-        echo json_encode($returnData);
-    }
-
-
-    public function obtenerOrdenEntrega()
-    {
-        $res = $this->ordenEntrega->ordenesEntrega();
-        return json_encode($res);
-    }
-
+    // public function detallesOrden($id)
+    // {
+    //     $returnData = array();
+    //     $ordenEntrega_ = $this->ordenEntrega->traerDetalles($id);
+    //     if (!empty($ordenEntrega_)) {
+    //         array_push($returnData, $ordenEntrega_);
+    //     }
+    //     echo json_encode($returnData);
+    // }
     public function buscarCate()
     {
         $tipoCate = $this->request->getPost('idTipoCate');
@@ -97,6 +77,75 @@ class OrdenEntrega extends BaseController
         } else {
             $res = $this->estanteria->traerBodega();
             return json_encode($res);
+        }
+    }
+    public function buscarMateriales()
+    {
+        $tipoMat = $this->request->getPost('tipo');
+        $id = $this->request->getPost('id');
+
+        if ($tipoMat == 9) {
+            $res = $this->materiales->obtenerInsumo($id);
+        } else {
+            $res = $this->materiales->obtenerRepuestoBodega($id);
+        }
+        return json_encode($res);
+    }
+
+    public function insertar()
+    {
+        $tp = $this->request->getPost('tp');
+        $idOrden = $this->request->getPost('id');
+        $ordenServicio = $this->request->getPost('ordenServicio');
+        $ordenesEnt = $this->request->getPost('ordenesEnt');
+        $trabajador = $this->request->getPost('trabajador');
+        $tipoMov = 68;
+        $fechaMov = date('Y-m-d');
+
+        $dataEncOrden = [
+            'id_vehiculo' => $ordenServicio,
+            'id_trabajador' => $trabajador,
+            'fecha_movimiento' => $fechaMov,
+            'tipo_movimiento' => $tipoMov,
+            'usuario_crea' => session('id')
+        ];
+
+        if ($tp == 2) {
+            // editar
+        } else {
+            if ($this->ordenEntrega->save($dataEncOrden)) {
+                return json_encode($this->ordenEntrega->getInsertID());
+            } else {
+                return json_encode(2);
+            }
+        }
+    }
+    public function insertarDet()
+    {
+        $tp = $this->request->getPost('tp');
+        $idMovEnc = $this->request->getPost('idMovEnc');
+        $idMat = $this->request->getPost('idMaterial');
+        $item = $this->request->getPost('item');
+        $cantidad = $this->request->getPost('cantidad');
+        $subtotal = $this->request->getPost('subtotal');
+
+        $dataDetMov = [
+            'id_movimientoenc' => $idMovEnc,
+            'id_material' => $idMat,
+            'item' => $item,
+            'cantidad' => $cantidad,
+            'costo' => $subtotal,
+            'usuario_crea' => session('id')
+        ];
+
+        if ($tp == 2) {
+            // editar
+        } else {
+            if ($this->movDet->save($dataDetMov)) {
+                return json_encode(1);
+            } else {
+                return json_encode(2);
+            }
         }
     }
 }
