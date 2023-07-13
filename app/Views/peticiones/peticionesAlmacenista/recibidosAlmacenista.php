0775d510fbcb22c9532a3d93529e208785190d51
Isabella <collantemendezisabella@gmail.com>
2023-07-12 20:46:35 -0500

    feat(peticiones): detalles estilos

--- app/Views/peticiones/peticionesAlmacenista/recibidosAlmacenista.php
+++ app/Views/peticiones/peticionesAlmacenista/recibidosAlmacenista.php
@@ -167,7 +167,7 @@
                     </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btnRedireccion" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btnAccionF" data-bs-dismiss="modal">Cerrar</button>
                 </div>
             </div>
         </div>
